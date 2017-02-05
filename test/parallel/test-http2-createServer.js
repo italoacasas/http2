661a9d9aaadd9ae6bcb669546ae5c191eaacf9b2
Italo A. Casas <me@italoacasas.com>
2017-02-04 21:42:06 -0500

    progress'

--- test/parallel/test-http2-createServer.js
+++ test/parallel/test-http2-createServer.js
@@ -4,30 +4,37 @@ const common = require('../common');
 const assert = require('assert');
 const http2 = require('http2');
 
-const object = {
-  domain: null,
-  _events: {},
-  _eventsCount: 2,
-  _maxListeners: undefined,
-  _connections: 0,
-  _handle: null,
-  _usingSlaves: false,
-  _slaves: [],
-  _unref: false,
-  allowHalfOpen: false,
-  pauseOnConnect: false,
-  timeout: 120000
-}
-
 // Handler (callback) is required.
-assert.throws( () => {
+assert.throws(() => {
     http2.createServer()
   }, /TypeError: handler must be a function/
 );
 
-const server = http2.createServer(() => {});
-const server1 = http2.createServer({}, () => {});
+/**
+ * createServer() accepts two params
+ * but only the callback is required.
+ */
+assert.doesNotThrow(() => {
+  http2.createServer(() => {})
+  http2.createServer({}, () => {})
+});
 
-console.log(server)
-console.log(object)
+const server = http2.createServer((stream, headers) => {
+  console.log('stream', stream);
+  console.log('header', headers);
+});
 
+server.emit('request', () => {
+  console.log('request')
+});
+
+assert.strictEqual(server.domain, null);
+assert.strictEqual(server.allowHalfOpen, false);
+assert.strictEqual(server.pauseOnConnect, false);
+assert.strictEqual(server.timeout, 120000);
+
+server.listen(8080, common.mustCall(() => {
+  console.log('listening')
+}));
+
+server.on('close', common.mustCall(() => {}));
